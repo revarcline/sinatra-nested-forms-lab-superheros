c06e84658689ee81e9aabd21e91deee67e1cb884
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-22 13:32:48 -0600

    commit tree 5ee01026be4bb8c3e1e307e3c495b4e570f632b5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, proc { File.join(root, '../views/') }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
 
+  post '/teams' do
+    @team = Team.new(params[:team])
 
+    params[:team][:members].each { |member| Member.new(member) }
+    @members = Member.all
+    erb :team
+  end
 end
